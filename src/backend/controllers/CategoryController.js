c5161b57e953f2612a3a58fd41dc6aa85549d51e
Mukesh Bishnoi <mukeshbishnoi@gmail.com>
2022-07-08 02:37:55 +0530

    feat: cra

---
+++ src/backend/controllers/CategoryController.js
@@ -0,0 +1,67 @@
+import { Response } from "miragejs";
+
+/**
+ * All the routes related to Category are present here.
+ * These are Publicly accessible routes.
+ * */
+
+/**
+ * This handler handles gets all categories in the db.
+ * send GET Request at /api/categories
+ * */
+
+export const getAllCategoriesHandler = function () {
+  try {
+    return new Response(200, {}, { categories: this.db.categories });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
+
+/**
+ * This handler handles gets all categories in the db.
+ * send GET Request at /api/user/category/:categoryId
+ * */
+
+export const getCategoryHandler = function (schema, request) {
+  const categoryId = request.params.categoryId;
+  try {
+    const category = schema.categories.findBy({ _id: categoryId });
+    return new Response(200, {}, { category });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
+
+export const getAllCatergoryQuizesHandler = function (schema, request) {
+  const categoryId = request.params.categoryId;
+
+  try {
+    const category = schema.categories.findBy({ _id: categoryId });
+    const catergoryQuizes = this.db.quizzes.filter(
+      (quiz) =>
+        quiz.catergoryName.toLowerCase() === category.categoryName.toLowerCase()
+    );
+    return new Response(200, {}, { quizzes: catergoryQuizes });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
